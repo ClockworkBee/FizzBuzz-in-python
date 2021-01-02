bce0f735afa4ddf38fb797a2de9ae233fcff345a
Rikk Hill <rikk@unashamedly.co.uk>
2021-01-02 16:17:58 +0000

    Made changes to Fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,10 @@
-x = 1
-for x in range (1,101):
-	if(x%3 == 0)and(x%5 == 0):
-		print("FizzBuzz \r")
-		continue
-	elif x%3==0:
-		print("Fizz \r")
-		continue
-	elif x%5==0:
-		print("Buzz \r")
-		continue
-	print(str(x) + "\r")
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
 
